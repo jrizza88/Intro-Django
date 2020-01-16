c85f89433cb2c42e8008999429df923f40a0ccb2
Jamar Torres <Jetorres88@gmail.com>
2020-01-15 19:00:20 -0500

    added decouple package

--- djangoExample/settings.py
+++ djangoExample/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k#xh4r!^2-+*jo^7(bu4hj7l%j+dani46n8+1#avd*h57sz35w'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
